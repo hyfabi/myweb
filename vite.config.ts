294a7e5e7e43777c800352006652023a24e3d005
Fabian Maurutschek <mau18927@spengergasse.at>
2022-12-05 15:26:28 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	server:{
+		host: "0.0.0.0",
+		port: 42069
+	},
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		alias: {
+		  '@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
